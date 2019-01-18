69e190c629ebcc3f59f3ca18bcb29eb29a8b72b4
Kateryna Burtseva <kateryna.burtseva@itera.no>
2019-01-18 17:30:36 +0200

    added folders to work with xml files

---
+++ XmlTaskAccounts/Accounts/Model/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Model
+{
+    [Serializable]
+    public class Account
+    {
+        public string FirstName { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public Account() {
+        }
+
+        public Account(string firstname, DateTime expirationDate)
+        {
+            FirstName = firstname;
+            ExpirationDate = expirationDate;
+        }
+        
+        
+}
